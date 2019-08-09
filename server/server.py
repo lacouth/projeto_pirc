2fc6b1edc5c7bd3cd9988415a8a183e273b74f0b
Patric Lacouth <lacouth@gmail.com>
2019-08-09 11:21:37 -0300

    Fix server.py

--- server/server.py
+++ server/server.py
@@ -13,7 +13,8 @@ from tcp_lib import TCP_SERVER
      
 
 if __name__ == "__main__":
-    tcp_server = TCP_SERVER("http://192.168.0.153:8080","",65432)
+    WEB_SERVER = "http://192.168.0.153:8080"
+    tcp_server = TCP_SERVER(WEB_SERVER,"",65432)
     
     web_conn = threading.Thread(target=tcp_server.verifica_estado)
     web_conn.start()
